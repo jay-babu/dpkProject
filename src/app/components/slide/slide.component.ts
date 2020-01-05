8e4f5a9c6d3e256742b74bad2fd9a1b123528e9f
jayp0521 <jay.r.patel2115@gmail.com>
2020-01-05 11:50:41 -0500

    Creating Slide Component

--- src/app/components/slide/slide.component.ts
+++ src/app/components/slide/slide.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-slide',
-  templateUrl: './slide.component.html',
-  styleUrls: ['./slide.component.css']
+    selector: 'app-slide',
+    templateUrl: './slide.component.html',
+    styleUrls: ['./slide.component.css']
 })
 export class SlideComponent implements OnInit {
+    @Input()
+    stanza: string[];
+    @Input()
+    definitions: string[];
+    @Input()
+    imageLocation: string;
 
-  constructor() { }
+    imageMaxHeight: number;
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
 
+    ngOnInit() {
+        this.imageMaxHeight = 100;
+        this.imageMaxHeightDecrement();
+    }
+
+    imageMaxHeightDecrement() {
+        /*
+        Changes the Max Height of Img in CSS.
+        4.5 was found to be a good number that worked for everything.
+         */
+        for (const index of this.stanza) {
+            this.imageMaxHeight -= 6;
+        }
+        for (const index of this.definitions) {
+            this.imageMaxHeight -= 6;
+        }
+    }
 }
