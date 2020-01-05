4c6ab8e10596008db1c1ab011a85b6c88b7a74c2
jayp0521 <jay.r.patel2115@gmail.com>
2020-01-04 23:56:17 -0500

    Adding in custom Material Theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularMaterialModule } from './angular-material.module';
 import { SlideComponent } from './components/slide/slide.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SlideComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SlideComponent
+    ],
+    imports: [
+        AngularMaterialModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule {
+}
