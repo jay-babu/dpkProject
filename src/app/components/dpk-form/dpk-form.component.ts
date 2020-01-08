6dadc51ee5c93d9d86951daad46b09108f288a35
jayp0521 <jay.r.patel2115@gmail.com>
2020-01-07 19:51:27 -0500

    Removing unused comments

--- src/app/components/dpk-form/dpk-form.component.ts
+++ src/app/components/dpk-form/dpk-form.component.ts
@@ -7,20 +7,11 @@ import { FormBuilder, Validators } from '@angular/forms';
     styleUrls: ['./dpk-form.component.css']
 })
 export class DpkFormComponent implements OnInit {
-    // n = 1;
-    //
     constructor(private fb: FormBuilder) {
-
     }
 
-    // dpkForm: FormGroup;
-    // Title = '';
-    // Lyrics = '';
-    // Definitions = '';
-    // ImagePaths = '';
     DPKs = ['Dhun', 'Prathana', 'Kirtan'];
 
-    // }
     dpkForm = this.fb.group({
         lyrics: ['', Validators.required],
         definitions: ['', Validators.required],
