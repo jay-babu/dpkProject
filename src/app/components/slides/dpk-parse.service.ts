30034d95993204ac401fb887c602fd4e8c20b134
jayp0521 <jay.r.patel2115@gmail.com>
2020-01-08 23:20:16 -0500

    Removing comments and unused function in dpk-parse.service.ts

--- src/app/components/slides/dpk-parse.service.ts
+++ src/app/components/slides/dpk-parse.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Bhajan, FirebaseBhajan } from '../../interfaces/bhajan';
+import { FirebaseBhajan } from '../../interfaces/bhajan';
 
 @Injectable({
     providedIn: 'root'
@@ -11,27 +11,7 @@ export class DpkParseService {
     }
 
     getDPK(dpk: string, name: string) {
-        // this.articles$ = this.fireDB.collection<Bhajan>(dpk)
-        //     .snapshotChanges().pipe(
-        //         map(actions => actions.map(a => {
-        //             const data = a.payload.doc.data() as Bhajan;
-        //             const title = a.payload.doc.id;
-        //             return {title, ...data};
-        //         }))
-        //     );
-        return this.fireDB.collection(dpk).doc<FirebaseBhajan>(name).valueChanges();
-        // this.articles$.subscribe(data => console.log(data));
-    }
 
-    fireDBToBhajan(data: any) {
-        if (data.definitions !== undefined && data.lyrics) {
-            // console.log(data.definitions);
-            let bhajan: Bhajan;
-            // bhajan.definitions = [for (const d of data.definitions) d.split(`\n`)];
-            bhajan.definitions = data.definitions.map(d => d.split(`\n`));
-            bhajan.lyrics = data.lyrics.map(d => d.split(`\n`));
-            bhajan.imageNames = data.imageNames;
-            return bhajan;
-        }
+        return this.fireDB.collection(dpk).doc<FirebaseBhajan>(name).valueChanges();
     }
 }
