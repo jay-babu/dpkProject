4b6915cbd9bc84997d84e9dedb96d866386c9d4b
jayp0521 <jay.r.patel2115@gmail.com>
2020-01-21 23:37:45 -0500

    Creating slider component that holds one card, such that title, imageLink, slidesLink, and line of bhajan are shown. line of bhajan may be removed. Seems unnecessary

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DpkFormComponent } from './components/dpk-form/dpk-form.component';
 import { PasswordlessAuthComponent } from './components/passwordless-auth/passwordless-auth.component';
 import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
+import { SliderItemsComponent } from './components/slider-items/slider-items.component';
 import { SlidesComponent } from './components/slides/slides.component';
 import { TempOptionsComponent } from './components/temp-options/temp-options.component';
 
@@ -12,7 +13,8 @@ const appRoutes: Routes = [
     {path: 'dpk/:dpk/:name', component: SlidesComponent},
     {path: 'options', component: TempOptionsComponent},
     {path: 'privacy-policy', component: PrivacyPolicyComponent},
-    {path: 'login', component: PasswordlessAuthComponent}
+    {path: 'login', component: PasswordlessAuthComponent},
+    {path: 'slides', component: SliderItemsComponent}
 ];
 
 export const AppRoutingModule: ModuleWithProviders = RouterModule.forRoot(appRoutes);
