d55e4c97cba49f5d1140ce1785ae9626c2989d75
jayp0521 <jay.r.patel2115@gmail.com>
2020-01-28 22:24:31 -0500

    SliderItem only provides DPK subset ID

--- src/app/components/slider-items/slider-items.component.ts
+++ src/app/components/slider-items/slider-items.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Slider } from '../../interfaces/slider';
+import { DriveAPIService } from '../../services/drive-api.service';
 
 @Component({
     selector: 'app-slider-items',
@@ -7,16 +7,22 @@ import { Slider } from '../../interfaces/slider';
     styleUrls: ['./slider-items.component.css']
 })
 export class SliderItemsComponent implements OnInit {
-    test: Slider = {
-        title: 'Sacha',
-        text: 'Cool',
-        imgLink: 'http://images.huffingtonpost.com/2016-04-20-1461151974-4400136-okay_2.jpg',
-        slidesLink: 'https://res.cloudinary.com/teepublic/image/private/s--VrZ6VhjH--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1522800257/production/designs/2556492_0.jpg'
-    };
+    DPKs: Map<string, string>;
 
-    constructor() {
+    constructor(private driveAPIService: DriveAPIService) {
     }
 
     ngOnInit() {
+        this.DPKItems();
+    }
+
+    async DPKItems() {
+        this.DPKs = new Map<string, any>();
+        await this.driveAPIService.getDPKRadio(`1NFdcrnJLViJgJyz9MiSxkCQOll3v5QnQ`).toPromise().then(DPKs => {
+            for (const DPK of DPKs.files) {
+                this.DPKs.set(DPK.name, DPK.id);
+            }
+        });
+        return this.DPKs;
     }
 }
