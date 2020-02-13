493113c7c0ebad0d923f13d703a67256cd598146
jayp0521 <jay.r.patel2115@gmail.com>
2020-02-12 23:02:30 -0500

    Implement routing to show sidebar on everything except slides and pdf

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,6 +5,7 @@ import { DpkFormComponent } from './components/dpk-form/dpk-form.component';
 import { PasswordlessAuthComponent } from './components/passwordless-auth/passwordless-auth.component';
 import { PdfSlideComponent } from './components/pdf-slide/pdf-slide.component';
 import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { SliderItemsComponent } from './components/slider-items/slider-items.component';
 import { SlidesComponent } from './components/slides/slides.component';
 import { TempOptionsComponent } from './components/temp-options/temp-options.component';
@@ -14,19 +15,27 @@ const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
 
 const appRoutes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'options' },
-    { path: 'login', component: PasswordlessAuthComponent },
     {
-        path: 'dpkCreate',
-        component: DpkFormComponent,
-        canActivate: [AngularFireAuthGuard],
-        data: { authGuardPipe: redirectUnauthorizedToLogin },
+        path: 'dpk', children: [
+            { path: 'pdf/:dpk/:name', component: PdfSlideComponent, },
+            { path: ':dpk/:name', component: SlidesComponent, },
+        ]
     },
-    { path: 'dpk/pdf/:dpk/:name', component: PdfSlideComponent },
-    { path: 'dpk/:dpk/:name', component: SlidesComponent },
-    { path: 'options', component: TempOptionsComponent },
-    { path: 'privacy-policy', component: PrivacyPolicyComponent },
-    { path: 'slides', component: SliderItemsComponent },
-    { path: 'test', component: TestComponent },
+    {
+        path: '', component: SidenavComponent, children: [
+            { path: 'login', component: PasswordlessAuthComponent, },
+            {
+                path: 'dpkCreate',
+                component: DpkFormComponent,
+                canActivate: [AngularFireAuthGuard],
+                data: { authGuardPipe: redirectUnauthorizedToLogin },
+            },
+            { path: 'options', component: TempOptionsComponent, },
+            { path: 'privacy-policy', component: PrivacyPolicyComponent, },
+            { path: 'slides', component: SliderItemsComponent, },
+            { path: 'test', component: TestComponent, },
+        ]
+    },
 ];
 
 export const AppRoutingModule: ModuleWithProviders = RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules });
