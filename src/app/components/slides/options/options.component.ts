99775b1917fe156534d225fd0069672c6888e168
jayp0521 <jay.r.patel2115@gmail.com>
2020-02-16 14:21:16 -0500

    Creating app-options-fab with timeout to show and hide button

---
+++ src/app/components/slides/options/options.component.ts
@@ -0,0 +1,26 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-options-fab',
+    templateUrl: './options.component.html',
+    styleUrls: ['./options.component.css']
+})
+export class OptionsComponent implements OnInit {
+    toggleButton: boolean;
+    timer: number;
+
+    @HostListener('document:mousemove', ['$event'])
+    onMouseMove() {
+        clearTimeout(this.timer);
+        this.toggleButton = true;
+        setTimeout(() => this.toggleButton = false, 1000);
+    }
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+        this.toggleButton = false;
+    }
+
+}
