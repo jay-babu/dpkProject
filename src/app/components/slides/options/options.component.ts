8316ea7c981c96b7979566c239d78f58f6b79d75
jayp0521 <jay.r.patel2115@gmail.com>
2020-02-16 22:12:19 -0500

    Creating dialog form. Results are logged to console upon changes. Pass observable to service to use in dpkSlides to implement changes

--- src/app/components/slides/options/options.component.ts
+++ src/app/components/slides/options/options.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-options',
@@ -6,11 +7,25 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./options.component.css']
 })
 export class OptionsComponent implements OnInit {
+    customForm: FormGroup;
+    fontStyles: Set<string>;
 
-    constructor() {
+    constructor(private fb: FormBuilder) {
     }
 
+
     ngOnInit(): void {
+        this.fontStyles = new Set(['Roboto', 'cursive', 'Avenir', 'Montserrat', 'Calibri', 'Helvetica Neue']);
+        this.customForm = this.fb.group({
+            bgColor: '',
+            fontStyle: '',
+            definitionShown: true,
+        });
+        this.onCustomFormChange();
+    }
+
+    onCustomFormChange() {
+        this.customForm.valueChanges.subscribe(form => console.log(form));
     }
 
 }
