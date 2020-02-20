7d63b3602139889e53ed50eb5fc3d4ca65c252fa
jayp0521 <jay.r.patel2115@gmail.com>
2020-02-20 00:22:08 -0500

    Creating observable in dpk-parse.service.ts. Unused right now

--- src/app/components/slides/dpk-parse.service.ts
+++ src/app/components/slides/dpk-parse.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { FirebaseBhajan } from '../../interfaces/bhajan';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class DpkParseService {
 
+    firebaseBhajan$: Observable<FirebaseBhajan>;
+
     constructor(private fireDB: AngularFirestore) {
     }
 
     getDPK(dpk: string, name: string) {
-        return this.fireDB.collection(dpk).doc<FirebaseBhajan>(name).valueChanges();
+        return this.firebaseBhajan$ = this.fireDB.collection(dpk).doc<FirebaseBhajan>(name).valueChanges();
     }
 
     parseSlideText(slideText: string[]) {
