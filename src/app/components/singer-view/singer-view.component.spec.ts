84ccafb9ae25e718e5c5697d80d436b2a827e4fb
jayp0521 <jay.r.patel2115@gmail.com>
2020-02-20 00:21:29 -0500

    Creating singer view

---
+++ src/app/components/singer-view/singer-view.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SingerViewComponent } from './singer-view.component';
+
+describe('SingerViewComponent', () => {
+  let component: SingerViewComponent;
+  let fixture: ComponentFixture<SingerViewComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SingerViewComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SingerViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
