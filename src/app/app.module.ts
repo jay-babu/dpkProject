f02b37660117c6e00c5d87832e11ad4bc95589e0
jayp0521 <jay.r.patel2115@gmail.com>
2020-02-24 09:55:40 -0500

    Splitting in sidenav.module and app.module. Everything is mostly now lazy loaded based on route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 // Firebase
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule, HammerModule } from '@angular/platform-browser';
@@ -14,27 +13,13 @@ import { AngularMaterialModule } from './angular-material.module';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
-import { DpkFormComponent } from './components/sidenav/dpk-form/dpk-form.component';
-import { DpkFormService } from './components/sidenav/dpk-form/dpk-form.service';
-import { LoginComponent } from './components/sidenav/login/login.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
-import { SliderItemsComponent } from './components/sidenav/slider-items/slider-items.component';
-import { SliderItemDirective } from './components/sidenav/slider-items/slider/slider-item.directive';
-import { SliderComponent } from './components/sidenav/slider-items/slider/slider.component';
-import { DpkParseService } from './components/dpk/slides/dpk-parse.service';
-import { DriveAPIService } from './services/drive-api.service';
 import { FirebaseService } from './services/firebase.service';
 import { SideNavToggleService } from './services/side-nav-toggle.service';
-import { ThemeService } from './services/theme.service';
 
 @NgModule({
     declarations: [
         AppComponent,
-        DpkFormComponent,
-        LoginComponent,
-        SliderComponent,
-        SliderItemDirective,
-        SliderItemsComponent,
         SidenavComponent,
     ],
     imports: [
@@ -51,13 +36,9 @@ import { ThemeService } from './services/theme.service';
         HammerModule,
     ],
     providers: [
-        AngularFireAuthGuard,
         FirebaseService,
-        DpkFormService,
-        DpkParseService,
-        DriveAPIService,
         SideNavToggleService,
-        ThemeService,
+
     ],
     bootstrap: [ AppComponent ],
 })
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,33 +1,13 @@
 import { ModuleWithProviders } from '@angular/core';
-import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { DpkFormComponent } from './components/sidenav/dpk-form/dpk-form.component';
-import { LoginComponent } from './components/sidenav/login/login.component';
-import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
-import { SliderItemsComponent } from './components/sidenav/slider-items/slider-items.component';
 
-const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
 
 const appRoutes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: 'slides' },
-    {
-        path: 'dpk', loadChildren: () => import('./components/dpk/dpk.module').then(m => m.DpkModule),
-    },
-    {
-        path: '', component: SidenavComponent, children: [
-            { path: 'login', component: LoginComponent, },
-            {
-                path: 'dpkCreate',
-                component: DpkFormComponent,
-                canActivate: [AngularFireAuthGuard],
-                data: { authGuardPipe: redirectUnauthorizedToLogin },
-            },
-            { path: 'privacy-policy', component: PrivacyPolicyComponent, },
-            { path: 'slides', component: SliderItemsComponent, },
-        ]
-    },
-    { path: '**', redirectTo: 'slides' }
+    {path: '', pathMatch: 'full', redirectTo: 'slides'},
+    {path: 'dpk', loadChildren: () => import('./components/dpk/dpk.module').then(m => m.DpkModule),},
+    {path: '', component: SidenavComponent, loadChildren: () => import('./components/sidenav/sidenav.module').then(m => m.SidenavModule),},
+    {path: '**', redirectTo: 'slides'}
 ];
 
-export const AppRoutingModule: ModuleWithProviders = RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules });
+export const AppRoutingModule: ModuleWithProviders = RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules});
