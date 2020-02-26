056eea0b2df2a4eb6da19012bc7f1f84617e42df
jayp0521 <jay.r.patel2115@gmail.com>
2020-02-26 15:09:32 -0500

    Updating options-fab.component.html to only go back to slides. Removes the need for slideService

--- src/app/components/dpk/slides/options-fab/options-fab.component.ts
+++ src/app/components/dpk/slides/options-fab/options-fab.component.ts
@@ -1,20 +1,17 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { SlideService } from '../../../../services/slide.service';
 import { OptionsComponent } from '../options/options.component';
 
 @Component({
     selector: 'app-options-fab',
     templateUrl: './options-fab.component.html',
-    styleUrls: ['./options-fab.component.css']
+    styleUrls: [ './options-fab.component.css' ]
 })
 export class OptionsFabComponent implements OnInit {
     toggleButton: boolean;
     timer: number;
 
-    prevLocation: string[];
-
-    @HostListener('document:mousemove', ['$event'])
+    @HostListener('document:mousemove', [ '$event' ])
     onMouseMove(e: MouseEvent) {
         if (window.innerHeight - e.clientY > 100 && window.innerWidth - e.clientX > 100) {
             this.timeClear();
@@ -26,12 +23,11 @@ export class OptionsFabComponent implements OnInit {
         }
     }
 
-    constructor(public dialog: MatDialog, private slideService: SlideService) {
+    constructor(public dialog: MatDialog,) {
     }
 
     ngOnInit(): void {
         this.toggleButton = false;
-        this.slideService.prevLocation$.subscribe(prevLocation => this.prevLocation = prevLocation);
     }
 
     timeClear() {
@@ -41,7 +37,7 @@ export class OptionsFabComponent implements OnInit {
     openDialog(): void {
         this.dialog.open(OptionsComponent, {
             width: '250px',
-            position: { right: '1%' },
+            position: {right: '1%'},
         });
     }
 }
