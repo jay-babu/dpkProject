e5cb69a794dd9792bec00881f42f89a45d8eda4a
jayp0521 <jay.r.patel2115@gmail.com>
2020-02-28 20:05:59 -0500

    Creating singerView Lottie Audio Fab

---
+++ src/app/components/dpk/singer-view/lottie-animated-audio-fab/lottie-animated-audio-fab.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LottieAnimatedAudioFabComponent } from './lottie-animated-audio-fab.component';
+
+describe('LottieAnimatedAudioFabComponent', () => {
+    let component: LottieAnimatedAudioFabComponent;
+    let fixture: ComponentFixture<LottieAnimatedAudioFabComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ LottieAnimatedAudioFabComponent ]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LottieAnimatedAudioFabComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
---
+++ src/app/components/dpk/singer-view/lottie-animated-audio-fab/lottie-animated-audio-fab.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-lottie-animated-audio-fab',
+    templateUrl: './lottie-animated-audio-fab.component.html',
+    styleUrls: [ './lottie-animated-audio-fab.component.css' ]
+})
+export class LottieAnimatedAudioFabComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
