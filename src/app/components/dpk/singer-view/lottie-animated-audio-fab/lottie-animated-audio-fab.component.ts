ee1a8436bfd7511369100a4d60031884aebff1de
jayp0521 <jay.r.patel2115@gmail.com>
2020-02-28 20:32:24 -0500

    animation plays on loop with hover. Includes seekPosition Function. audioControl injected here now

--- src/app/components/dpk/singer-view/lottie-animated-audio-fab/lottie-animated-audio-fab.component.ts
+++ src/app/components/dpk/singer-view/lottie-animated-audio-fab/lottie-animated-audio-fab.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AnimationOptions } from 'ngx-lottie';
+import { AnimationDirection, AnimationItem } from 'lottie-web';
+import { AudioControlService } from '../../../audio-component/audio-control.service';
 
 @Component({
     selector: 'app-lottie-animated-audio-fab',
@@ -7,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LottieAnimatedAudioFabComponent implements OnInit {
 
-    constructor() {
+    @Input() time: number;
+
+    options: AnimationOptions = {
+        path: './assets/playPause.json',
+        autoplay: false,
+        loop: true,
+        rendererSettings: {
+            viewBoxSize: '100 700 880 10',
+        }
+    };
+
+    direction: AnimationDirection = -1;
+
+    animation: AnimationItem;
+
+    constructor(private audioControlService: AudioControlService) {
     }
 
     ngOnInit(): void {
     }
 
+    animationCreated(animationItem: AnimationItem) {
+        this.animation = animationItem;
+    }
+
+    seekPosition(time: number) {
+        this.audioControlService.seekTime(time);
+    }
 }
