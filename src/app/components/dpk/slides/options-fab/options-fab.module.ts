59a5eb36501c877c8d3e8aec87f1b3b3f1ec8e41
jayp0521 <jay.r.patel2115@gmail.com>
2020-03-12 19:50:43 -0400

    Creating audio, options-fab, singer-view modules and declaring components related to it

---
+++ src/app/components/dpk/slides/options-fab/options-fab.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OptionsFabComponent } from './options-fab.component';
+import { LottieAudioFabComponent } from './lottie-audio-fab/lottie-audio-fab.component';
+import { LottieLangFabComponent } from './lottie-lang-fab/lottie-lang-fab.component';
+import { OptionsComponent } from '../options/options.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterModule } from '@angular/router';
+import { TutorialModule } from '../../../tutorial/tutorial.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { LottieModule } from 'ngx-lottie';
+
+@NgModule({
+    declarations: [
+        OptionsFabComponent,
+        LottieAudioFabComponent,
+        LottieLangFabComponent,
+        OptionsComponent,
+    ],
+    imports: [
+        CommonModule,
+        MatIconModule,
+        MatTooltipModule,
+        RouterModule,
+        TutorialModule,
+        MatButtonModule,
+        MatDialogModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatSlideToggleModule,
+        LottieModule,
+    ],
+    exports: [
+        OptionsFabComponent,
+    ],
+})
+export class OptionsFabModule {
+}
