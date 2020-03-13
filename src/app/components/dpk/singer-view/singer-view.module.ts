4d3d1b5f40a8300b70df51a9290230844bd1a2c7
jayp0521 <jay.r.patel2115@gmail.com>
2020-03-12 20:13:42 -0400

    Removing matButtonModule

--- src/app/components/dpk/singer-view/singer-view.module.ts
+++ src/app/components/dpk/singer-view/singer-view.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { SingerViewComponent } from './singer-view.component';
 import { LottieAnimatedAudioFabComponent } from './lottie-animated-audio-fab/lottie-animated-audio-fab.component';
-import { OptionsFabModule } from '../slides/options-fab/options-fab.module';
+import { OptionsFabModule } from '../options-fab/options-fab.module';
 import { AudioModule } from '../../audio-component/audio.module';
 import { LottieModule } from 'ngx-lottie';
-import { MatButtonModule } from '@angular/material/button';
 
 const singerViewRoutes: Routes = [
     { path: ':dpk/:name', component: SingerViewComponent }
@@ -23,7 +22,6 @@ const singerViewRoutes: Routes = [
         OptionsFabModule,
         RouterModule.forChild(singerViewRoutes),
         LottieModule,
-        MatButtonModule,
     ]
 })
 export class SingerViewModule {
