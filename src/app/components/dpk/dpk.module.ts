8280eadf789f064136c9b81d37f7152bde0ea2c1
jayp0521 <jay.r.patel2115@gmail.com>
2020-03-12 20:14:19 -0400

    Removing unnecessary modules in dpkModule and moving to slide.module

--- src/app/components/dpk/dpk.module.ts
+++ src/app/components/dpk/dpk.module.ts
@@ -1,40 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { SlidesComponent } from './slides/slides.component';
-import { SlideComponent } from './slides/slide/slide.component';
-import { AngularMaterialModule } from '../../angular-material.module';
-import { ReactiveFormsModule } from '@angular/forms';
 import { AudioControlService } from '../audio-component/audio-control.service';
 import { SlideService } from '../../services/slide.service';
 import { DriveAPIService } from '../../services/drive-api.service';
-import { LottieModule } from 'ngx-lottie';
-import { ToggleFullScreenDirective } from '../../directive/toggle-full-screen.directive';
-import { TutorialModule } from '../tutorial/tutorial.module';
-import { AudioModule } from '../audio-component/audio.module';
-import { OptionsFabModule } from './slides/options-fab/options-fab.module';
 
 const dpkRoutes: Routes = [
     // { path: 'pdf/:dpk/:name', component: PdfSlideComponent, },
-    { path: 'slideShow/:dpk/:name', component: SlidesComponent, },
+    { path: 'slideShow', loadChildren: () => import('./slides/slides.module').then(m => m.SlidesModule), },
     { path: 'singerView', loadChildren: () => import('./singer-view/singer-view.module').then(m => m.SingerViewModule), },
 ];
 
 @NgModule({
-    declarations: [
-        SlidesComponent,
-        SlideComponent,
-        ToggleFullScreenDirective,
-    ],
+    declarations: [],
     imports: [
-        AudioModule,
         CommonModule,
         RouterModule.forChild(dpkRoutes),
-        AngularMaterialModule,
-        ReactiveFormsModule,
-        LottieModule,
-        TutorialModule,
-        OptionsFabModule,
     ],
     providers: [
         AudioControlService,
