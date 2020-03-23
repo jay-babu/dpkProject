4c3863b680b8cb78ac82def83871940921faf95f
jayp0521 <jay.r.patel2115@gmail.com>
2020-03-23 10:39:58 -0400

    youtube-blur.component.ts is ready to go

--- src/app/components/dpk/slides/slide/youtube-blur/youtube-blur.component.ts
+++ src/app/components/dpk/slides/slide/youtube-blur/youtube-blur.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { YoutubeService } from '../youtube/youtube.service';
 
 @Component({
     selector: 'app-youtube-blur',
@@ -7,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class YoutubeBlurComponent implements OnInit {
 
-    constructor() {
+    videoId: string;
+
+    constructor(public youtubeService: YoutubeService) {
     }
 
     ngOnInit(): void {
+        // This code loads the IFrame Player API code asynchronously, according to the instructions at
+        // https://developers.google.com/youtube/iframe_api_reference#Getting_Started
+        const tag = document.createElement('script');
+
+        tag.src = 'https://www.youtube.com/iframe_api';
+        document.body.appendChild(tag);
+
+        this.youtubeService.youtubeId$.subscribe(videoId => this.videoId = videoId);
     }
 
+    muted(event: YT.PlayerEvent) {
+        event.target.mute();
+        event.target.playVideo();
+        event.target.setLoop(true);
+    }
+
+    width() {
+        return window.innerWidth * 1.5;
+    }
+
+    height() {
+        return window.innerWidth * 1.5;
+    }
 }
--- src/app/components/dpk/slides/slide/youtube-blur/youtube-blur.module.ts
+++ src/app/components/dpk/slides/slide/youtube-blur/youtube-blur.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { YoutubeBlurComponent } from './youtube-blur.component';
+import { YouTubePlayerModule } from '@angular/youtube-player';
 
 
 @NgModule({
     declarations: [
         YoutubeBlurComponent,
     ],
+    exports: [
+        YoutubeBlurComponent
+    ],
     imports: [
-        CommonModule
+        CommonModule,
+        YouTubePlayerModule
     ]
 })
 export class YoutubeBlurModule {
