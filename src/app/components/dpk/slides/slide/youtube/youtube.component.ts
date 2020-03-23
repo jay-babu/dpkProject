e52c2315a2389e371c5bb4ccf8c30232754c977f
jayp0521 <jay.r.patel2115@gmail.com>
2020-03-23 00:50:56 -0400

    Placing in video in component and calling service

--- src/app/components/dpk/slides/slide/youtube/youtube.component.ts
+++ src/app/components/dpk/slides/slide/youtube/youtube.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { YoutubeService } from './youtube.service';
 
 @Component({
     selector: 'app-youtube',
@@ -7,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class YoutubeComponent implements OnInit {
 
-    constructor() {
+    videoId: string;
+
+    constructor(public youtubeService: YoutubeService) {
     }
 
     ngOnInit(): void {
+        // This code loads the IFrame Player API code asynchronously, according to the instructions at
+        // https://developers.google.com/youtube/iframe_api_reference#Getting_Started
+        const tag = document.createElement('script');
+
+        tag.src = 'https://www.youtube.com/iframe_api';
+        document.body.appendChild(tag);
+
+        this.youtubeService.youtubeId$.subscribe(videoId => this.videoId = videoId);
+    }
+
+
+    muted(event: YT.PlayerEvent) {
+        event.target.mute();
+    }
+
+    width() {
+        return 320 * 3;
+    }
+
+    height() {
+        return 180 * 3;
     }
 
 }
