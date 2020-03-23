40502864080bcf976adad89ace29fbb65780805b
jayp0521 <jay.r.patel2115@gmail.com>
2020-03-22 23:40:30 -0400

    Creating youtube service and import

--- src/app/components/dpk/slides/slide/youtube/youtube.module.ts
+++ src/app/components/dpk/slides/slide/youtube/youtube.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { YoutubeComponent } from './youtube.component';
+import { YoutubeService } from './youtube.service';
 
 
 @NgModule({
-    declarations: [ YoutubeComponent ],
+    declarations: [ YoutubeComponent, ],
     imports: [
         CommonModule
+    ],
+    providers: [
+        YoutubeService,
     ]
 })
 export class YoutubeModule {
