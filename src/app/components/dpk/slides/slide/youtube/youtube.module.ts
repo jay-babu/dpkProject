c25ca77feab833d1b763680ea80d69f4cddf49c7
jayp0521 <jay.r.patel2115@gmail.com>
2020-03-22 23:39:01 -0400

    Creating youtube.component

---
+++ src/app/components/dpk/slides/slide/youtube/youtube.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { YoutubeComponent } from './youtube.component';
+
+
+@NgModule({
+    declarations: [ YoutubeComponent ],
+    imports: [
+        CommonModule
+    ]
+})
+export class YoutubeModule {
+}
