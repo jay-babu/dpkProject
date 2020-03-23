672a1ece65c28dfedfd7aeb447aa1e5530b2fd86
jayp0521 <jay.r.patel2115@gmail.com>
2020-03-23 10:15:32 -0400

    Creating youtube-blur component and module

---
+++ src/app/components/dpk/slides/slide/youtube-blur/youtube-blur.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-youtube-blur',
+    templateUrl: './youtube-blur.component.html',
+    styleUrls: [ './youtube-blur.component.css' ]
+})
+export class YoutubeBlurComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
---
+++ src/app/components/dpk/slides/slide/youtube-blur/youtube-blur.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { YoutubeBlurComponent } from './youtube-blur.component';
+
+
+@NgModule({
+    declarations: [
+        YoutubeBlurComponent,
+    ],
+    imports: [
+        CommonModule
+    ]
+})
+export class YoutubeBlurModule {
+}
